10a117acf9b1d1fd0523066b14b21e0620d6a243
artem.bratchykov <ledger318@gmail.com>
2021-04-19 13:21:21 +0300

    Added base project

---
+++ dotNet/Battlecity/Demo/Program.cs
@@ -0,0 +1,51 @@
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2018 - 2021 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Battlecity.API;
+
+namespace Demo
+{
+    class Program
+    {
+        static string serverURL = "https://epam-botchallenge.com/codenjoy-contest/board/player/vsw86l76vx5va61b7ju4?code=8749211683513820687";
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth - 50, Console.LargestWindowHeight - 10);
+
+            // Creating custom AI client
+            var bot = new YourSolver(serverURL);
+
+            // Starting thread with playing game
+            Task.Run(bot.Play);
+            Console.ReadLine();
+
+            // On any key - asking AI client to stop.
+            bot.InitiateExit();
+        }
+    }
+}
---
+++ dotNet/Battlecity/FormUI/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Demo;
+
+namespace FormUI
+{
+    public partial class Form1 : Form
+    {
+        static string serverURL = "https://epam-botchallenge.com/codenjoy-contest/board/player/vsw86l76vx5va61b7ju4?code=8749211683513820687";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Creating custom AI client
+            var bot = new YourSolver(serverURL);
+            bot.RoundCallbackHandler += SetText;
+
+            // Starting thread with playing game
+            Task.Run(bot.Play);
+
+            
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.label1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label1.Text = text;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
